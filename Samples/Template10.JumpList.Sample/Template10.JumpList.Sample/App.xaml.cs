a7873349079e5023965a54b580972d4199086dce
tonychampion <tonyc@championds.com>
2019-10-15 14:31:17 -0500

    JumpList activation sample

---
+++ Samples/Template10.JumpList.Sample/Template10.JumpList.Sample/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Prism.Ioc;
+using System;
+using System.Threading.Tasks;
+using Template10.Ioc;
+using Template10.Navigation;
+using Template10.Services;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Template10.JumpList.Sample.Views;
+using Template10.JumpList.Sample.ViewModels;
+using Windows.UI.StartScreen;
+using Windows.ApplicationModel.Activation;
+
+namespace Template10.JumpList.Sample
+{
+    sealed partial class App : Template10.ApplicationBase
+    {
+        private INavigationService _nav;
+
+        public App() => InitializeComponent();
+
+        public override void RegisterTypes(IContainerRegistry container)
+        {
+            container.RegisterView<MainPage, MainPageViewModel>();
+        }
+
+        public override void OnInitialized()
+        {
+            var frame = new Frame();
+            Window.Current.Content = new ShellPage { Frame = frame };
+            Window.Current.Activate();
+            _nav = NavigationFactory
+                .Create(frame, Guid.Empty.ToString())
+                .AttachGestures(Window.Current, Gesture.Back, Gesture.Forward, Gesture.Refresh);
+        }
+
+        public override async Task OnStartAsync(IStartArgs args)
+        {
+            await CreateJumpList();
+
+            if (args.StartKind == StartKinds.Activate 
+                && args.StartCause == StartCauses.JumpListItem
+                && args.Arguments is LaunchActivatedEventArgs)
+            {
+                var jumpListArgs = (args.Arguments as LaunchActivatedEventArgs).Arguments;
+                var navParams = new NavigationParameters(("key", jumpListArgs));
+
+                await _nav.NavigateAsync(nameof(MainPage), navParams);
+            } else
+            {
+                await _nav.NavigateAsync(nameof(MainPage));
+            }
+        }
+
+        private async Task CreateJumpList()
+        {
+            var jumpList = await Windows.UI.StartScreen.JumpList.LoadCurrentAsync();
+
+            if(jumpList.Items.Count == 0)
+            {
+                jumpList.Items.Add(JumpListItem.CreateWithArguments("RecentFile1", "Recent File 1"));
+                jumpList.Items.Add(JumpListItem.CreateWithArguments("RecentFile2", "Recent File 2"));
+                jumpList.Items.Add(JumpListItem.CreateWithArguments("RecentFile3", "Recent File 3"));
+
+                await jumpList.SaveAsync();
+
+            }
+
+        }
+    }
+}
---
+++ Samples/Template10.JumpList.Sample/Template10.JumpList.Sample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Navigation;
+
+namespace Template10.JumpList.Sample.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase, IMainPageViewModel
+    {
+        private string _title;
+        public string Title {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if(parameters.ContainsKey("key"))
+            {
+                Title = String.Format("Loaded JumpList Key - {0}", parameters["key"]);
+            } else
+            {
+                Title = "Template 10 JumpList Demo";
+            }
+        }
+    }
+}
